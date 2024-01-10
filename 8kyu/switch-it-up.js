204efee7afd201acaa0421b42db4f1a541b3c1ac
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-11 00:44:38 +0300

    add  switch-it-up.js

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,54 @@
+//When provided with a number between 0-9, return it in words.
+//
+//Input :: 1
+//
+//Output :: "One".
+//
+//If your language supports it, try using a switch statement.
+
+// Solution:
+function switchItUp(number) {
+	switch (true) {
+		case number === 1:
+			return ('One');
+			break;
+		case (number === 3):
+			return ('Three');
+			break;
+		case (number === 5):
+			return ('Five');
+			break;
+		default:
+	}
+}
+
+console.log(switchItUp(1));
+
+//function switchItUp(number){
+//	switch(number) {
+//		case 0:
+//			return 'Zero';
+//		case 1:
+//			return 'One';
+//		case 2:
+//			return 'Two';
+//		case 3:
+//			return 'Three';
+//		case 4:
+//			return  'Four';
+//		case 5:
+//			return 'Five';
+//		case 6:
+//			return 'Six';
+//		case 7:
+//			return 'Seven';
+//		case 8:
+//			return 'Eight';
+//		case 9:
+//			return 'Nine';
+//		default:
+//			return 'Unknown number';
+//	}
+//}
+//
+//console.log(switchItUp(2));
