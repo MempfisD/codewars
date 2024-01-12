292f362f3bafcab0a0ef606034db0ae1a8cf768a
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-12 13:46:56 +0300

    add logic-drills-traffic-light.js

--- 8kyu/logic-drills-traffic-light.js
+++ 8kyu/logic-drills-traffic-light.js
@@ -9,13 +9,10 @@ const updateLight = (current) => {
 	switch (current) {
 		case('green'):
 			return ('yellow');
-			break;
 		case('yellow'):
 			return ('red');
-			break;
 		case('red'):
 			return ('green');
-			break;
 		default:
 			console.log('Wrong');
 	}
