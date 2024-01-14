2252d96c8d2fcde377cf19f89129525ddf8824ce
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-14 11:26:47 +0300

    add safen-user-htmlspecialchars.js

---
+++ 8kyu/safen-user-htmlspecialchars.js
@@ -0,0 +1,16 @@
+//Mission
+//Your mission is to implement a function that converts the following potentially harmful characters:
+//
+//	< --> &lt;
+//> --> &gt;
+//" --> &quot;
+//& --> &amp;
+
+//Solution:
+function htmlspecialchars(formData) {
+	return formData
+		.replace(/&/g, '&amp;')
+		.replace(/"/g, '&quot;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;');
+}
