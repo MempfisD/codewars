92c3c1fe87fb7c59d2230912d17384cf059ddfc5
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-18 18:03:21 +0300

    add square(n)-sum.js

---
+++ 8kye_1/square(n)-sum.js
@@ -0,0 +1,10 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// Solution:
+function squareSum(numbers) {
+	return numbers
+		.map(n => n * n)
+		.reduce((total, n) => {
+			return total + n;
+		}, 0);
+}
