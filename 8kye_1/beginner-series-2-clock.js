68829e3df98912d6a57484380236f971112ce32f
Dmitry Naumov <komrad.nda@yandex.ru>
2024-01-21 22:26:13 +0300

    add beginner-series-2-clock_1.js

--- 8kye_1/beginner-series-2-clock.js
+++ 8kye_1/beginner-series-2-clock.js
@@ -3,6 +3,14 @@
 //	Your task is to write a function which returns the time since midnight in milliseconds.
 
 // Solution:
+//function past(h, m, s) {
+//	return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+//}
+
 function past(h, m, s) {
-	return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+	let hours = h * 60 * 60 * 1000;
+	let minutes = m * 60 * 1000;
+	let seconds = s * 1000;
+
+	return hours + minutes + seconds;
 }
